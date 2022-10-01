33024f499d4239920f937bef03c6928548cf6c49
srivithyadevi <114822727+srivithyadevi@users.noreply.github.com>
2022-10-01 23:23:56 +0530

    Add files via upload
    assign 2

---
+++ day/mobile.java
@@ -0,0 +1,19 @@
+package week1.day;
+
+public class mobile {
+
+	public static void main(String[] args) {
+		
+String mobilemodel= "samsung";
+ float mobileweight=1.5f;
+ boolean fullcharged=true;
+ int mobilecost=8500;
+ 
+
+System.out.println("mobilemodel is:" +mobilemodel);
+System.out.println("mobileweight:" +mobileweight);
+System.out.println("fullcharged is:" +fullcharged);
+System.out.println("mobilecost is" +mobilecost);
+	}
+
+}
